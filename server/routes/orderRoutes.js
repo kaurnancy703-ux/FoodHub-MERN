9c30c3ead509326e9d61bccb8195abe987388e85
my-second-project <kaurnancy703@gmail.com>
2026-08-10 20:56:08 +0530

    Fix my orders route

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const {
   createOrder,
+  getMyOrders,
 } = require("../controllers/orderController");
 
 const router = express.Router();
 
 // Create FoodHub order
 router.post("/", createOrder);
+
+// Get logged-in user's orders
+router.get("/my-orders", getMyOrders);
 
 module.exports = router;
